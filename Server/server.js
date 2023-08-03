04ab9a2bc9013758e77d125f48d639aae5f99b6c
Marcos Siqueira <106640310+Msiqueira1@users.noreply.github.com>
2023-08-03 08:15:29 -0300

    Evolução 1

--- Server/server.js
+++ Server/server.js
@@ -26,7 +26,15 @@ db.connect(function (err) {
 })
 
 app.post('/login', (req, res) => {
-
+  const sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+  db.query(sql, [req.body.username, req.body.password], (err, result) => {
+    if(err) return res.json({Status: "Error", Error: "Error in running query"});
+    if(result.length > 0) {
+      return res.json({Status: "Success"})
+    } else {
+      return res.json({Status: "Error", Error: "Wrong Username or Password"});
+    }
+  })
 })
 
 
